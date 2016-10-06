3c2fa8490603177ec41b7ef3a1e64d621e4d7322
sujith857 <sujith857@gmail.com>
2016-10-06 12:58:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,6 +79,12 @@ var htmltemplate=`
 return htmltemplate;
     
 }
+var counter=0
+app.get('/counter',function (req,res){
+    counter=counter+1;
+    res.send(counter.toString());
+    
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
