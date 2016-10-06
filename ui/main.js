7feedd4f584c7b37b87cebe7db64a745ef154554
sujith857 <sujith857@gmail.com>
2016-10-06 15:42:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,20 +1,23 @@
 
 var button=document.getElementById('counter');
 
-button.onclick = function()
-    {
-        var request=new XMLHttpRequest();//create a request 
+button.onclick = function(){
+    
+var request=new XMLHttpRequest();//create a request 
         request.onreadystatechange=function()
         {
-            if(request.readystatechange===200)
+            if(request.readystate===XMLRequest.DONE)
             {
+                if(request.status===200)
+                {
                 var counter=request.responseText;
                  var span =document.getElementById('count');
                   span.innerHTML=counter.toString();
             
             }
-        };
+        }
        request.open('GET','http://sujith857.imad.hasura-app.io/counter',true);
        request.response(null);
         
     };
+};
