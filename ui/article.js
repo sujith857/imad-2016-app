e5d6fe1e79863e55590f5589f70be8c8533d763b
sujith857 <sujith857@gmail.com>
2016-10-27 13:10:10 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -1,1 +1,32 @@
+var commentBtn = document.getElementById('comment_btn');
+commentBtn.onclick = function (){
+   var commentInput = document.getElementById('comment');
+    var comment = commentInput.value;
+    console.log('comment is : ',comment);
+    
+
+    var request = new XMLHttpRequest();
+
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                var comments = request.responseText;
+                console.log('comments1 is : ',comments);
+                comments = JSON.parse(comments);
+                console.log('comments is : ',comments);
+                var comment_list='';
+                for(var i=0;i<comments.length;i++){
+                    var t=i+1;
+                    comment_list+='<p>'+'comment '+t+': '+comments[i]+'</p>'+ '<hr>' ;
+                }
+                var commentz = document.getElementById('comments');
+                commentz.innerHTML = comment_list;
+            }
+            
+        }
+
+       
+    };
+   request.open('GET','http://silentarrowz.imad.hasura-app.io/submit_comment?comment='+comment,true);
+   request.send(null);
 
